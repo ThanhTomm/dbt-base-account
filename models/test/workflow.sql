43d1ffc7fbeffcc7cbf1a0ed482bcba6c7b72f7a
Thanh Dang <thanh.dang@base.vn>
2023-06-27 07:50:54 +0000

    0.0.0

--- models/test/workflow.sql
+++ models/test/workflow.sql
@@ -1,3 +1,10 @@
 {{ config(materialized='table') }}
 
-SELECT * FROM `base-datateam.dataset_test.workflow` WHERE TIMESTAMP_TRUNC(_airbyte_emitted_at, DAY) = TIMESTAMP("2023-06-26") LIMIT 1000
+SELECT 
+    w.id
+    , w.name
+    , w.status
+    , w.last_update
+    , wd.category
+    FROM `base-data-analyst.dataset_test.workflow` w
+    JOIN `base-data-analyst.dataset_test.workflow_data` wd on w._airbyte_workflows 
